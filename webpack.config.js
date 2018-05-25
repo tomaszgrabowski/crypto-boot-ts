414f11d97e397c2735f7e286b5868000f8c0a184
Tomasz Grabowski <tomaszgrabowski07@gmail.com>
2018-05-25 21:11:55 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+module.exports = function (env) {
+    var isProduction = env === 'production';
+    return {
+        entry: './src/app.tsx',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'index.js'
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        module: {
+            rules: [{
+                loader: 'ts-loader',
+                test: /\.tsx$/,
+                exclude: /node_modules/
+            }]
+        },
+        // devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        // devServer: {
+        //     contentBase: path.join(__dirname, 'public')
+        // }
+    };
+};
+//# sourceMappingURL=webpack.config.js.map
