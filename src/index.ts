8ea876596cad49ade1e8e555bee1248c5ad338b5
Tomasz Grabowski <tomaszgrabowski07@gmail.com>
2018-05-27 15:05:12 +0200

    basic routes established

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import app from "./App";
-const port = 3000;
-app.listen(port, ()=> {
-  console.log('Express server listening on port ' + port);
+import * as express from "express";
 
-  app.get('/',(req, res)=>{
-    res.json('dupa');
+const port = process.env.PORT || 3000;
+const app: express.Application = express();
+let router: express.Router = express.Router();
+
+app.use(router);
+
+app.listen(port, () => {
+
+  console.log(`Crypto-boot has started, app is listenting on port: ${port}`);
+
+  router.get('/', (req: express.Request, res: express.Response) => {
+    res.json("dupa");
+  });
+
+  router.get('/privacy', (req: express.Request, res: express.Response) => {
+    //rodo
+  });
+
+  router.get('/', (req: express.Request, res: express.Response) => {
+    //verification process is taken place here
+  });
+
+  router.post('/', (req: express.Request, res: express.Response) => {
+    //users request for information are handled here
   })
+
 });
