d7a33b99124b21f705579665df9bd6c3927d45b0
Tomasz Grabowski <tomaszgrabowski07@gmail.com>
2018-06-20 11:57:03 +0200

    heroku stuff

--- src/RequestSourceValidator.ts
+++ src/RequestSourceValidator.ts
@@ -3,6 +3,7 @@ import IRequestSourceValidator from './interfaces/IRequestSourceValidator'
 
 export default class RequestSourceValidator implements IRequestSourceValidator {
     validate(req: express.Request): boolean {
+        console.log(req);
         return req.body.object === 'page';
     }
 }
